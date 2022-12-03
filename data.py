184dd58f175de469e0c7df73db52e7d7695b064c
Geoff Mendoza <112456980+gmendzy@users.noreply.github.com>
2022-12-02 20:17:35 -0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,66 @@
+encounter_0 = {
+'pos' : (25,   380), 
+'order':1,
+'encounter_graphic': 'graphics\Skull.png' }
+encounter_1 = {
+'pos' : (90,  320),
+ 'order':2,
+ 'encounter_graphic': 'graphics\Skull.png' }
+encounter_2 = {
+'pos' : (155, 260), 
+'order':3,
+'encounter_graphic': 'graphics\Eye.png',
+'string_list': ["As you continue your climb up the steep mountain,", "you feel an evil presence watching over you. What do you do?", "Run for cover?",
+"Continue walking and ignore the evil presence?", "You feel invigorated by your choice. HP + 5", "You feel your vitality drained by an evil force. HP - 5" ],
+"hp_bonus": [5, -5] }
+encounter_3 = {
+'pos' : (220, 200), 
+'order':4,
+'encounter_graphic': 'graphics\Skull.png' }
+encounter_4 = {
+'pos' : (285, 140),
+ 'order':5,
+ 'encounter_graphic': 'graphics\Skull.png' }
+encounter_5 = {
+'pos' : (350, 80),
+'order':6,
+ 'encounter_graphic': 'graphics\Eye.png' ,
+ 'string_list': ["As you continue your climb up the steep mountain,", "you feel an evil presence watching over you. What do you do?", "Run for cover?",
+"Continue walking and ignore the evil presence?", "You feel invigorated by your choice. HP + 5", "You feel your vitality drained by an evil force. HP - 5" ],
+"hp_bonus": [5, -5] }
+encounter_6 = {
+'pos' : (415, 20), 
+'order':7,
+'encounter_graphic': 'graphics\Skull.png' }
+encounter_7 = {
+'pos' : (480, 80), 
+'order':8,
+'encounter_graphic': 'graphics\Skull.png' }
+encounter_8 = {
+'pos' : (545, 140), 
+'order':9,
+'encounter_graphic': 'graphics\Eye.png',
+'string_list': ["As you continue your climb up the steep mountain,", "you feel an evil presence watching over you. What do you do?", "Run for cover?",
+"Continue walking and ignore the evil presence?", "You feel invigorated by your choice. HP + 5", "You feel your vitality drained by an evil force. HP - 5" ],
+"hp_bonus": [5, -5] } 
+encounter_9 = {
+'pos' : (610, 200),
+'order':10,
+'encounter_graphic': 'graphics\Skull.png'}
+encounter_10 = {
+'pos' : (675, 260), 
+'order':11,
+'encounter_graphic': 'graphics\Skull.png' }
+
+encounters = {
+    0: encounter_0,
+    1: encounter_1,
+    2: encounter_2,
+    3: encounter_3,
+    4: encounter_4,
+    5: encounter_5,
+    6: encounter_6,
+    7: encounter_7,
+    8: encounter_8,
+    9: encounter_9,
+    10: encounter_10,}
